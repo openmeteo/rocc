44be228e5c3059f8c30d798573bc6e5541ce6bd4
Antonis Christofides <antonis@antonischristofides.com>
2023-12-20 13:23:21 +0200

    Allow htimeseries 6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["htimeseries>=4,<5"]
+requirements = ["htimeseries>=4,<7"]
 
 setup_requirements = ["cython>=0.29,<0.30"]
 
